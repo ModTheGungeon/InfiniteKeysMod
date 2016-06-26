ae73a76ef2b0b5e3b9086c976cceae53bde35eab
Zatherz <zatherz@linux.pl>
2016-06-27 00:29:07 +0200

    Initial commit

---
+++ InfKeysMod/InfKeys.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+// This class is the "main" mod class. ETGMod will run the methods inside it as required.
+// The name and version is in the metadata.txt in the .zip, not here just as with the backends.
+public class InfKeysMod : ETGModule {
+
+	public InfKeysMod() {
+		/* Initialization happens in Start! */
+	}
+
+	public override void Start() {
+	}
+
+	public override void Update() {
+		// Update code goes in here.
+		ETGMod.Player.InfiniteKeys = true;
+		Console.WriteLine (ETGMod.Player.InfiniteKeys.ToString ());
+	}
+
+	public override void Exit() {
+		// Disposal code goes in here.
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # InfiniteKeysMod
 Simple mod that lets you open all locks forever for free.
+
+# Install
+Copy `InfKeysMod.zip` to Mod the Gungeon's `Mods` directory.
+
+# Dependencies
+* ETGMod version ???

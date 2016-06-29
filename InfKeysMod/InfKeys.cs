ab49e051a5ead4201ee0209085886f3aec05ebc7
Zatherz <zatherz@linux.pl>
2016-06-29 17:15:03 +0200

    Add ETGMod as submodule

--- InfKeysMod/InfKeys.cs
+++ InfKeysMod/InfKeys.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using UnityEngine;
 
 // This class is the "main" mod class. ETGMod will run the methods inside it as required.
 // The name and version is in the metadata.txt in the .zip, not here just as with the backends.
@@ -13,6 +14,8 @@ public class InfKeysMod : ETGModule {
 
 	public override void Update() {
 		// Update code goes in here.
+		Vector3 test = new Vector3(0, 0, 0);
+		Debug.Log(test.ToString());
 		ETGMod.Player.InfiniteKeys = true;
 		Console.WriteLine (ETGMod.Player.InfiniteKeys.ToString ());
 	}
